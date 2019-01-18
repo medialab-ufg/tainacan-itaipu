5a4eb19e370c3c29cebb1bd7dca8f210ce750278
Fabiano Alencar <fabianocarvalho15@gmail.com>
2019-01-18 10:27:56 -0200

    Initializing the child theme

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles', 99 );
+function my_theme_enqueue_styles() {
+	wp_dequeue_style('tainacan_tainacanStyle'); // This remove the repeat parent style
+
+    $parent_style = 'tainacan-interface'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+ 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'itaipu',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+?>
